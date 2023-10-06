8b673c373b79cd93f2c5ce2a9448f54df3da9047
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-10-06 01:16:14 +0000

    4-define_variables.py

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Define variables
+Just some variables definitions
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
