11f15a1f031bbeb18afb2eb75baa1f39ef52ee3b
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-10-06 00:54:46 +0000

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - concat
+ALX SE example for doctstring with str or Text type
+"""
+
+
+def concat(a: str, b: str) -> str:
+    """
+    Concatenates two strings.
+    """
+    return a + b
