bc5d5bb5c9876922ae4d15c40c7fc9e83066f80c
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-10-06 01:01:22 +0000

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - floor
+ALX SE example for doctstring with str or Text type
+"""
+
+
+def floor(x: float) -> int:
+    """
+    Returns the floor of a float.
+    """
+    return int(x)
