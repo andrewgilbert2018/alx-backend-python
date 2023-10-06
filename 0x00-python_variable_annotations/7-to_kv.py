71f8dd592eafb1a8c18a892bbf51240b83076850
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-10-06 01:41:12 +0000

    7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -10,4 +10,4 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Converts k str and v, to key value pairs
     """
-    return (k, float(v**2)
+    return (k, float(v**2))
