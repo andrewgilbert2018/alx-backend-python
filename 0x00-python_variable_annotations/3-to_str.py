0674368efda500309e635cfa3d8b9644f53c766f
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-10-06 01:12:06 +0000

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - to string
+Converts float to string data type
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts float to string
+    """
+    return str(n)
