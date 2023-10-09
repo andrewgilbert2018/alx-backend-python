f0853407a65c0a973fe9172ba3c5253757e7c7a4
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-10-09 15:19:31 +0000

    4-tasks.py

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+A python script that
+Concurrent coroutines using async wait and asyncio.gather
+"""
+from asyncio import gather
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """
+    Wait for a random amount of time
+    """
+    gather_list = await gather(
+        *[task_wait_random(max_delay) for _ in range(n)]
+        )
+    return gather_list
