57970ebc65d0329a0a69d0d64f0c4b99ef8d60cc
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-10-09 15:49:25 +0000

    4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,16 +2,17 @@
 """
 Concurrent coroutines using async wait and asyncio.gather
 """
-from asyncio import gather
+import asyncio
 from typing import List
+
+
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Executes task_wait_random n times.
     """
-    Wait for a random amount of time
-    """
-    gather_list = await gather(
-        *[task_wait_random(max_delay) for _ in range(n)]
-        )
-    return gather_list
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
